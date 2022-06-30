12f8b118bf8e954af5cacdfa85e2b6aa9ec364a7
unknown <mdtauhidulislam509@gmail.com>
2022-06-30 20:18:59 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Component/Header/Header';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Component/Home/Home';
+import CompleteTask from './Component/CompleteTask/CompleteTask';
+import ToDoList from './Component/ToDoList/ToDoList';
+import Calender from './Component/Calender/Calender';
+import About from './Component/About/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Header></Header>
+     <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/completetask" element={<CompleteTask />} />
+        <Route path="/todolist" element={<ToDoList />} />
+        <Route path="calender" element={<Calender />} />
+       
+      </Routes>
     </div>
   );
 }
